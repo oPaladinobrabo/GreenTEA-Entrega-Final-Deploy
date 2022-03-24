3581a9fafff2bda1d8a0b157bac9bba20b394121
zailua <84216764+zailua@users.noreply.github.com>
2022-03-24 05:53:39 -0300

    agora vai

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import Header from './Components/Header'
 import Footer from './Components/Footer'
+import './site.css'
 
 ReactDOM.render(
-  <React.StrictMode>
-          <Header/>
-      <Footer/>
-  </React.StrictMode>,
-  document.getElementById('root')
+	<React.StrictMode>
+		<Header />
+		<Footer />
+	</React.StrictMode>,
+	document.getElementById('root')
 )
